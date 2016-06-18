3f9c9a060f4c48a75de047d72162b69ad4a61055
uu <giladbarkan@live.com>
2016-06-18 08:08:29 -0400

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,19 @@
 def starts_with_a_vowel?(word)
-
+  if /^[aeiou]/i.match(word) then true else false end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\bun\w+|\w+ing\b/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if /^[A-Z].+[.!?]$/.match(text) then true else false end
 end
 
 def valid_phone_number?(phone)
-
+  if /^\s*\(\d{3}\)[ -]*\d{3}[ -]*\d{4}\s*$|^\s*\d{3}[ -]*\d{3}[ -]*\d{4}\s*$/.match(phone) then true else false end
 end
