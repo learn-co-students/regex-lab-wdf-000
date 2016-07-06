c3bee49c5b08649c7faeb386132f5fa23cd0be54
blaze.i.burgess@gmail.com <blaze.i.burgess@gmail.com>
2016-07-06 13:49:27 -0400

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,20 @@
 def starts_with_a_vowel?(word)
-
+  !!(word.match(/^[aeiou]\w*/i))
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.split.grep(/un\w*ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.split.grep(/^\w{5}$/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  !!(text.match(/^[A-Z].*[\.\?\!]$/))
 end
 
 def valid_phone_number?(phone)
-
+  p phone
+  phone.chars.grep(/\d/).length == 10
 end
